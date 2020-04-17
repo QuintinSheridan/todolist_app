da32291d078c8f95da8da22ac39414ebb392d2f2
qsherida <qsherida@LAPTOP-27TPG5EM.localdomain>
2020-04-16 20:18:36 -0600

    update added to todo items

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request, jsonify
+import sys
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -24,6 +25,7 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
@@ -33,15 +35,39 @@ class Todo(db.Model):
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.get_json()['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  body={}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exec_info())
+  finally:
+    db.session.close()
+  if not error:
+    return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def update_completed(todo_id):
+  print(f'todo_id: {todo_id}')
+  try:
+    print('trying to get completed')
+    completed = request.get_json()['completed']
+    print(f'completed: {completed}')
+    todo = Todo.query.get(todo_id)
+    print(f'todo.completed: {todo.completed}')
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.order_by('id').all())
